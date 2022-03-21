0472a560e0241e4c2451f7020ac5dc16a3ea933b
유민상 <minsang105@naver.com>
2022-03-21 22:36:00 +0900

    26강 scaffolding_aflutter_app

---
+++ scaffolding_a_flutter_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Text('I Am Rich'),
+      ),
+    ),
+  ));
+}
