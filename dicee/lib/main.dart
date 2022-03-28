ac363343ea7945fd3ffbb3ed88c608cdc5c65334
유민상 <minsang105@naver.com>
2022-03-28 11:53:38 +0900

    56강 Dicee Variables

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,6 +20,10 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 빌드 메소드 밖에서 변수를 선언하면 페이지 클래스에서 반영되지 않는다.
+    var leftDiceNumber = 5;
+    var rightDiceNumber = 3;
+
     return Center(
       child: Row(
         children: [
@@ -29,10 +35,12 @@ class DicePage extends StatelessWidget {
               // FlatButton 위젯은 단순한 버튼이다.
               // FlatButton 자체에 내부여백이 존재한다. (오른쪽과 왼쪽)
               // FlatButton은 Flutter2.0버전에서는 바뀌어서 쓰이지 않는다.
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              // 문자, 문자열을 표시할 때 ' ' 혹은 " " 을 사용한다. 따옴표 안에 $변수명을 사용하면 변수값을 출력할 수 있다.
               onPressed: () {
                 // 버튼을 눌렀을 경우 해당 부분을 실행하게 된다.
-                // ()는 void callback 이다.
+                // onPressed 함수는 void callback 요구하는 함수이다.
+                // void callback은 anonymouse function(익명 함수)를 요구한다.
                 print('Left Button got Pressed');
                 // print문은 문자, 문자열을 출력하는 역할을 하지만 Dart 외 다른 언어에서도 에러를 확인할 때  쓰인다.
               },
@@ -41,7 +49,7 @@ class DicePage extends StatelessWidget {
           ),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
               onPressed: () {
                 print('Right Button got pressed');
               },
