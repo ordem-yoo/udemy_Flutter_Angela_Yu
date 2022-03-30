80ef52c66175dad8ee6f3136c88067373b1288e5
유민상 <minsang105@naver.com>
2022-03-30 16:16:16 +0900

    Magic 8 Ball Challenge

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,47 +1,52 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.blueAccent.shade200,
-          appBar: AppBar(
-            title: Text('Ask Me Anything'),
-          ),
-          body: BallPage(),
-        ),
+        home: BallPage(),
       ),
     );
 
-class BallPage extends StatefulWidget {
-  // 화ㅁㄴ의 유동적 변화를 위한 StatefulWidget 사용
+class BallPage extends StatelessWidget {
   @override
-  State<BallPage> createState() => _BallPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent.shade200,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
 }
 
-class _BallPageState extends State<BallPage> {
-  int changeBall = 1;
-  // build 메서드를 피한 변수 선언
+class Ball extends StatefulWidget {
+  // 앱 UI의 유동적 변화를 위해서 StatefulWidget 사용
+  @override
+  State<Ball> createState() => _BallState();
+}
 
-  void Changeball() {
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void changeBallNumber() {
     setState(() {
-      changeBall = Random().nextInt(5) + 1;
+      ballNumber = Random().nextInt(5) + 1;
     });
+    print('I got clicked');
+    print('ballNumber = $ballNumber');
   }
-  // onpressed 안 함수의 내용
 
   @override
   Widget build(BuildContext context) {
+    // build 메서드를 피한 변수 선언
     return Center(
       child: FlatButton(
         onPressed: () {
-          Changeball();
-          // 함수 제작
+          changeBallNumber();
         },
-        child: Image.asset('images/ball$changeBall.png'),
-        // String interpolation 사용
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
