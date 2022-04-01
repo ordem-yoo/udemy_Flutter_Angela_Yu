d3228c5db8472af5b5b5e16aef83face3315fa59
유민상 <minsang105@naver.com>
2022-04-02 01:07:15 +0900

    82강 Xylophone Refactoring Our App

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -22,55 +22,42 @@ class XylophoneApp extends StatelessWidget {
     // pubspec.yaml에서 asset은 이미지, 텍스트, 오디오 파일에 대한 설정을 할 수 있다.
   }
 
+  // 아래와 같이 위젯도 함수를 써서 반복되는 위젯의 코드를 축약할 수 있다.
+  // 하지만 함수로 만들기 앞서 해야할 과정이 있다.
+  // 1. 문장의 끝이 ;로 끝난다.
+  // 2. 함수의 return typr에 void가 있으면 안된다.
+  //    임의의 함수를 만든다고하면 return 값이 있는 함수를 만들 수 있고,
+  //    return 값이 없는 함수를 만들수 있는데 return값이 없는 함수는 void를 사용해도 되지만
+  //    return 값이 있는 경우 return값에 따라 type을 정해줘야한다.
+  //    밑에 있는 함수도 return 값이 있기 때문에 type을 return값에 맞춰 type을 정해줘야 한다.
+  Expanded XyloTile(int n) {
+    return Expanded(
+      flex: 1,
+      child: FlatButton(
+        color: Colors.red,
+        onPressed: () {
+          Key(n);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  Key(1);
-                },
-              ),
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  Key(2);
-                },
-              ),
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  Key(3);
-                },
-              ),
-              FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  Key(4);
-                },
-              ),
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  Key(5);
-                },
-              ),
-              FlatButton(
-                color: Colors.indigo,
-                onPressed: () {
-                  Key(6);
-                },
-              ),
-              FlatButton(
-                color: Colors.purple,
-                onPressed: () {
-                  Key(7);
-                },
-              ),
+              XyloTile(1),
+              XyloTile(2),
+              XyloTile(3),
+              XyloTile(4),
+              XyloTile(5),
+              XyloTile(6),
+              XyloTile(7),
             ],
           ),
         ),
