0d90e0a450c9af61951ad0627def76823b40d367
유민상 <minsang105@naver.com>
2022-04-02 13:25:04 +0900

    85강 Xylophone App Complete

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -12,6 +12,8 @@ import 'package:audioplayers/audioplayers.dart';
 // 예를 들어 ^0.20.1 버전이 있으면  0.20.1 <= 범위 < 1.0.0 의 범위를 가지는 것과 같다.
 
 void main() => runApp(XylophoneApp());
+// => 는 {return}과 동일하다.
+// 1 줄만 사용 가능하고 return 값이 없는 void에도 사용가능하다.
 
 class XylophoneApp extends StatelessWidget {
   // 코드 간략화를 위한 함수 제작
@@ -25,18 +27,27 @@ class XylophoneApp extends StatelessWidget {
   // 아래와 같이 위젯도 함수를 써서 반복되는 위젯의 코드를 축약할 수 있다.
   // 하지만 함수로 만들기 앞서 해야할 과정이 있다.
   // 1. 문장의 끝이 ;로 끝난다.
-  // 2. 함수의 return typr에 void가 있으면 안된다.
+  // 2. 함수의 return Type에 void가 있으면 안된다.
   //    임의의 함수를 만든다고하면 return 값이 있는 함수를 만들 수 있고,
   //    return 값이 없는 함수를 만들수 있는데 return값이 없는 함수는 void를 사용해도 되지만
-  //    return 값이 있는 경우 return값에 따라 type을 정해줘야한다.
-  //    밑에 있는 함수도 return 값이 있기 때문에 type을 return값에 맞춰 type을 정해줘야 한다.
-  Expanded XyloTile(int n) {
+  //    return 값이 있는 경우 return값에 따라 Type을 정해줘야한다.
+  //    밑에 있는 함수도 return 값이 있기 때문에 Type을 return값에 맞춰 Type을 정해줘야 한다.
+  // 3. property가 있는 위젯을 함수로 만들려면 인자를 입력하는 () 안에 {Type property}로 정의해줘야 한다.
+  //    이해를 위해 아래 코드를 해석해보자면
+  //    build 메서드 안 XyloTile 함수를 보면 () 안에 property들이 있다.
+  //    property에 hover해보면 Type이 나온다.
+  //    이 Type을 아래 XyloTile 함수의 인자에 넣는다.
+  //    {Type property} 이런식으로 넣으면 된다.
+  //    그리고 아래 함수에는 build 메서드 안의 함수에서 작성했던 property를 넣어주면된다.
+  //    한마디로 property가 변수 역할을 하게되는 것이다.
+  Expanded XyloTile({Color color, int keyNumber}) {
+    int n;
     return Expanded(
       flex: 1,
       child: FlatButton(
-        color: Colors.red,
+        color: color,
         onPressed: () {
-          Key(n);
+          Key(keyNumber);
         },
       ),
     );
@@ -51,13 +62,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              XyloTile(1),
-              XyloTile(2),
-              XyloTile(3),
-              XyloTile(4),
-              XyloTile(5),
-              XyloTile(6),
-              XyloTile(7),
+              XyloTile(color: Colors.red, keyNumber: 1),
+              XyloTile(color: Colors.orange, keyNumber: 2),
+              XyloTile(color: Colors.yellow, keyNumber: 3),
+              XyloTile(color: Colors.green, keyNumber: 4),
+              XyloTile(color: Colors.blue, keyNumber: 5),
+              XyloTile(color: Colors.indigo, keyNumber: 6),
+              XyloTile(color: Colors.purple, keyNumber: 7),
             ],
           ),
         ),
