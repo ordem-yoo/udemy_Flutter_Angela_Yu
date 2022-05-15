30567be113023ba8f3028d99fdcd813b68b41717
Minsang Yoo <minsang105@naver.com>
2022-05-15 21:25:28 +0900

    94. Creating a Question Class

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,11 +1,18 @@
+// Package
 import 'package:flutter/material.dart';
 
-void main() => runApp(Quizzler());
+// Page
+import 'question.dart';
+
+void main() => runApp(const Quizzler());
 
 class Quizzler extends StatelessWidget {
+  const Quizzler({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
@@ -20,6 +27,8 @@ class Quizzler extends StatelessWidget {
 }
 
 class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
   @override
   _QuizPageState createState() => _QuizPageState();
 }
@@ -34,29 +43,38 @@ class _QuizPageState extends State<QuizPage> {
   // dynamic을 통해 data의 type을 유동적으로 받는 변수를 만들 수 있지만 에러발생 가능성이 크다.
   // 따라서 유동적인 data type이 필요한 경우가 아니라면 명시하는게 좋다.
   // 아래에서 <Icon>을 해도 된다. []안의 위젯이 전부 Icon이기 때문에 가능한것이다.
-  List<Widget> scoreIcon = [
-    // 아이콘 위젯 FontAwesome Package를 통해서 좋은 아이콘을 사용할 수 있다.
-    Icon(
-      Icons.check_box,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.check_box,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.check_box,
-      color: Colors.green,
-    ),
+
+  List<Icon> scoreIcon = [];
+  // 정답 표시
+
+  // List<String> questions = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.',
+  // ]; // 특수문자 중 다른 역할로 사용되고 있는 특수문자들은 \(역슬래시) 사용해서 표현한다.
+  // // 문제 목록
+
+  // List<bool> answers = [
+  //   false,
+  //   true,
+  //   true,
+  // ];
+  // // 정답 목록
+
+  // // 문제와 정답을 한 번에 저장하기 위해서 클래스를 생성했다.
+  // // question.dart 참조
+  // Question q1 = Question(
+  //   q: 'You can lead a cow down stairs but not up stairs.',
+  //   a: false,
+  // );
+
+  List<Question> questionBank = [
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
   ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -69,7 +87,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -81,11 +99,11 @@ class _QuizPageState extends State<QuizPage> {
         ),
         Expanded(
           child: Padding(
-            padding: EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(15.0),
             child: FlatButton(
               textColor: Colors.white,
               color: Colors.green,
-              child: Text(
+              child: const Text(
                 'True',
                 style: TextStyle(
                   color: Colors.white,
@@ -93,35 +111,70 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                // 버튼 값 변수에 저장
+                bool correctAnswer =
+                    questionBank[questionNumber].questionAnswer;
+
+                // 정답 확인
+                if (correctAnswer == true) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
+
+                setState(() {
+                  scoreIcon.add(
+                    const Icon(
+                      Icons.check_box,
+                      color: Colors.green,
+                    ),
+                  );
+                  questionNumber = questionNumber + 1;
+                });
               },
             ),
           ),
         ),
         Expanded(
           child: Padding(
-            padding: EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(15.0),
             child: FlatButton(
               color: Colors.red,
-              child: Text(
+              child: const Text(
                 'False',
                 style: TextStyle(
                   fontSize: 20.0,
                   color: Colors.white,
                 ),
               ),
-              // 버튼을 눌렀을 때 {} 내용을 수행하게 하는 함수
+              // 버튼을 눌렀을 때 {}안 코드를 수행하게 하는 함수
               onPressed: () {
                 // setState는 이전에 설명한 것처럼 build method를 호출하는 함수이다.
                 // setState가 없으면 계속 build를 하지 못해 콘솔에서만 내용이 남는다.
+
+                // 버튼 값 변수에 저장
+                bool correctAnswer =
+                    questionBank[questionNumber].questionAnswer;
+
+                // 정답 확인
+                if (correctAnswer == true) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
+
                 setState(
                   () {
                     // List에서 data를 추가하려면 Collection명.add를 사용한다.
                     // 반대로 지우려면 Collection명.removeAt(index number)로 지운다.
-                    scoreIcon.add(Icon(
-                      Icons.check_box,
-                      color: Colors.green,
-                    ));
+                    scoreIcon.add(
+                      const Icon(
+                        // 아이콘 위젯 FontAwesome Package를 통해서 좋은 아이콘을 사용할 수 있다.
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                    questionNumber++; // questionNumber = questionNumber + 1 코드를 줄일수 있다.
                   },
                 );
               },
@@ -129,15 +182,11 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         // scoreIcon은 List type으로 된 Collection이다.
-        // Collection이 children의 []역할을 대신하므로 children: scoreIcon이 문제가 없다.
-        Row(children: scoreIcon),
+        // Collection이 children의 []역할을 대신하므로 children: scoreIcon이 가능하다.
+        Row(
+          children: scoreIcon,
+        )
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
