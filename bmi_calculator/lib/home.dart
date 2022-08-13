53fb4e78ce974bb6208c871984eef57a457c8365
Minsang Yoo <minsang105@naver.com>
2022-08-13 23:11:03 +0900

    Widget Refactoring & Fontawesome

--- bmi_calculator/lib/home.dart
+++ bmi_calculator/lib/home.dart
@@ -1,5 +1,6 @@
 // Package
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
@@ -20,8 +21,22 @@ class _HomeState extends State<Home> {
             // Expanded 위젯은 위젯의 크기를 핸드폰 화면에 맞춰서 생성해주는 위젯
             child: Row(
               children: [
-                Expanded(child: ReusableCard(colour: activeCardColour)),
-                Expanded(child: ReusableCard(colour: activeCardColour)),
+                Expanded(
+                    child: ReusableCard(
+                  colour: activeCardColour,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: "MALE",
+                  ),
+                )),
+                Expanded(
+                    child: ReusableCard(
+                  colour: activeCardColour,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    label: "FEMALE",
+                  ),
+                )),
                 // Extract Widget을 통해서 반복되는 위젯을 함수처럼 축약할 수 있다.
                 // 위젯 일부만 변경하기 위해 프로퍼티를 추가했다면 null 값을 쓸 수 없다.
               ],
@@ -47,15 +62,47 @@ class _HomeState extends State<Home> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  IconContent({this.icon, this.label});
+
+  final IconData? icon;
+  final String? label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          FontAwesomeIcons.mars,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          'MALE',
+          style: TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98)),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
   // const ReusableCard({Key? key,}) : super(key: key);
   // 이 프로젝트에서는 사용 안함
   // key 클래스는 위젯의 상태를 추적할 때 사용한다.
   // 위젯이 위젯 트리에서 움직일 때, 위젯이 바뀔 때 유용하다.
 
-  ReusableCard({required this.colour}); // 위젯의 일부 프로퍼티를 변경할 때 작성해 일부만 바꿀 수 있다.
+  ReusableCard(
+      {required this.colour,
+      this.cardChild}); // 위젯의 일부 프로퍼티를 변경할 때 작성해 일부만 바꿀 수 있다.
+  // 위젯의 필수 프로퍼티일 경우에는 required 키워드를 쓴다.
 
   final Color colour; // 프로퍼티 만들기
+  final Widget?
+      cardChild; // required가 없는 프로퍼티일 경우 null 값으로 설정이 되는데 null-Safety를 위해 위젯 이름 뒤에 ?를 쓴다.
 
   /*
     final vs const
@@ -66,6 +113,7 @@ class ReusableCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       // color: Color(0xff1d1e33), // Boxdecoration과 같이 쓸경우 오류가 발생한다.
       // Boxdecoration을 사용할 경우 Boxdecoration안에서 color 프로퍼티로 색상을 지정해주면 된다.
       margin: EdgeInsets.all(15.0),
