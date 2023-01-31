0182d471ec1605b3ab126163205a12087453c2d7
Minsang Yoo <minsang105@naver.com>
2023-01-31 22:08:09 +0900

    .gitignore fix

--- i_am_rich_app/android/app/src/main/java/com/example/how_to_add_app_icons_to_the_ios_and_android_apps/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.how_to_add_app_icons_to_the_ios_and_android_apps;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
