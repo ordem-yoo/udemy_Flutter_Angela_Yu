29e6041577a6cebb927a382892c748f329737c7f
Minsang Yoo <minsang105@naver.com>
2023-08-09 17:01:51 +0900

    complete gold challenge

--- bitcoin_ticker/lib/coin_data.dart
+++ bitcoin_ticker/lib/coin_data.dart
@@ -36,30 +36,41 @@ const List<String> cryptoList = [
 ];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apikey = 'CB26E771-4D89-44FA-98A9-FF1DC38D616E';
+const apikey = 'INPUT YOUR API KEY';
 
 class CoinData {
   // Silver 3. getCoinData를 업데이트하기 위해 selectcurrency를 함수 인자로 사용하기
   // Bronze 3. Future를 반환하는 비동기 메서드 getCoinDate() 만들기
   Future getCoinData(String selectedCurrency) async {
-    // Silver 4. 선택한 통화 입력을 사용하기 위해 URL 업데이트
-    // Bronze 4. 관심있는 코인을 coinAPIURL과 결합하기
-    String requestURL = '$coinAPIURL/BTC/$selectedCurrency?apikey=$apikey';
-    // Bronze 5. URL에 GET 요청을 하고 response 대기
-    Response response = await get(Uri.parse(requestURL));
-    // Bronze 6. 요청이 되었는지 확인
-    if (response.statusCode == 200) {
-      // Bronze 7-2. 'dart:convert' 패키지를 사용해서 coinAPI.io에서 가져온 JSON 데이터를 decode
-      var decodeData = jsonDecode(response.body);
-      // Bronze 8. 'last' 키로 bitcoin의 마지막 가격을 가져오기
-      var lastPrice = decodeData['rate'];
-      // Bronze 9. 메서드에서 마지막 가격 출력
-      return lastPrice;
-    } else {
-      // Bronze 10. 요청 중 발생한 오류 처리
-      print(response.statusCode);
-      // Bronze Optional: 요청 실패 시 오류 발생
-      throw 'Problem with the get request';
+    // Gold 5. 단일 값 대신 Map을 반환
+    Map<String, String> cryptoPrices = {};
+    // Gold 4. for문을 사용하여 CryptoList와 각각의 데이터를 차례로 요청하기
+    for (String crypto in cryptoList) {
+      // for문 의미 = cryptoList에서 0번째 부터 crypto라는 변수에 넣어 반복문을 동작 시킬것임
+      // Silver 4. 선택한 통화 입력을 사용하기 위해 URL 업데이트
+      // Bronze 4. 관심있는 코인을 coinAPIURL과 결합하기
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apikey';
+      // Bronze 5. URL에 GET 요청을 하고 response 대기
+      Response response = await get(Uri.parse(requestURL));
+      // Bronze 6. 요청이 되었는지 확인
+      if (response.statusCode == 200) {
+        // Bronze 7-2. 'dart:convert' 패키지를 사용해서 coinAPI.io에서 가져온 JSON 데이터를 decode
+        var decodeData = jsonDecode(response.body);
+        // Bronze 8. 'last' 키로 bitcoin의 마지막 가격을 가져오기
+        var lastPrice = decodeData['rate'];
+        // 새로운 키-값 쌍을 만들어 준다. 키는 암호화폐 명칭, 값은 암호화폐의 가격
+
+        // // Bronze 9. 메서드에서 마지막 가격 출력
+        // return lastPrice;
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(2);
+      } else {
+        // Bronze 10. 요청 중 발생한 오류 처리
+        print("statusCode : ${response.statusCode}");
+        // Bronze Optional: 요청 실패 시 오류 발생
+        throw 'Problem with the get request';
+      }
     }
+    return cryptoPrices;
   }
 }
--- bitcoin_ticker/lib/price_screen.dart
+++ bitcoin_ticker/lib/price_screen.dart
@@ -91,17 +91,31 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  // Bronze 12. TextWidget에서 값을 유지하고 사용할 수 있는 변수를 만들고, 데이터가 비동기 메서드에 돌아오기 전에 변수에 '?'로 시작 값 지정
-  String bitcoinValue = '?';
+  // Gold 6. getData 메서드를 수정하여 crypto:price 값 쌍이 포함된 Map을 받도록 합니다. 그리고 Map을 사용하여 CryptoCard를 업데이트 함
+  // bitcoinValue에서 coinValues로 변경
+  // // Bronze 12. TextWidget에서 값을 유지하고 사용할 수 있는 변수를 만들고, 데이터가 비동기 메서드에 돌아오기 전에 변수에 '?'로 시작 값 지정
+  // String bitcoinValue = '?';
+  Map<String, String> coinValues = {};
+
+  // Gold 7. price data가 오기 전 '?'를 표시하는 방법 구현하기 (hint : 3항 연산자)
+  // Gold 7-1. 요청 완료를 기다리는 동안 추적할 변수를 만들어야 합니다.
+  bool isWaiting = false;
 
   // Bronze 11. coin.dart로부터 coindata를 await하는 비동기 메서드 만들기
   void getData() async {
+    // Gold 7-2. 가격 요청 시 true로 설정
+    isWaiting = true;
     try {
-      double data = await CoinData().getCoinData(selectedCurrency);
+      // Gold 6. getData 메서드를 수정하여 crypto:price 값 쌍이 포함된 Map을 받도록 하기 그리고 Map을 사용하여 CryptoCard를 업데이트 함
+      var data = await CoinData().getCoinData(selectedCurrency);
+      // Gold 7-3. 위 코드가 완료되면 기다리지 않아도 되므로 isWaiting을 false로 변경함
+      isWaiting = false;
       // Bronze 13. setState()에서 await을 할 수 없기 때문에 두 단계로 분리
       setState(() {
-        // toStringAsFixed는 소수점 개수를 고정하는 함수
-        bitcoinValue = data.toStringAsFixed(0);
+        // // toStringAsFixed는 소수점 개수를 고정하는 함수
+        // bitcoinValue = data.toStringAsFixed(0);
+        coinValues = data;
+        print("coinValues : $coinValues");
       });
     } catch (e) {
       print(e);
@@ -115,7 +129,7 @@ class _PriceScreenState extends State<PriceScreen> {
     getData();
   }
 
-  // // for statement challenge
+  // // for loop challenge
   // List<DropdownMenuItem<String>> androidDropdown() {
   //   List<DropdownMenuItem<String>> dropdownItems = [];
   //   for (String currency in currenciesList) {
@@ -129,19 +143,21 @@ class _PriceScreenState extends State<PriceScreen> {
   //   return dropdownItems;
   // }
 
-  // 안드로이드, IOS 플랫폼에 맞게 알아서 드롭다운을 가져옵니다.
-  Widget getPicker() {
-    if (Platform.isIOS) {
-      return iOSPicker();
-    } else if (Platform.isAndroid) {
-      return androidDropdown();
-    } else {
-      // The body might complete normally, causing 'null' to be returned, but the return type, 'Widget', is a potentially non-nullable type.
-      // Try adding either a return or a throw statement at the end. 문제가 발생해서 else에 androidDropdown을 한번 더 추가했다.
-      return androidDropdown();
-    }
-  }
-  // 3항 연산을 통해 플랫폼 분류를 할 수 있다.
+  // // 안드로이드, IOS 플랫폼에 맞게 알아서 드롭다운을 가져옵니다.
+  // Widget getPicker() {
+  //   if (Platform.isIOS) {
+  //     return iOSPicker();
+  //   } else if (Platform.isAndroid) {
+  //     return androidDropdown();
+  //   } else {
+  //     // The body might complete normally, causing 'null' to be returned, but the return type, 'Widget', is a potentially non-nullable type.
+  //     // Try adding either a return or a throw statement at the end. 문제가 발생해서 else에 androidDropdown을 한번 더 추가했다.
+  //     return androidDropdown();
+  //   }
+  // }
+  // // 3항 연산을 통해 플랫폼 분류를 할 수 있다.
+
+  // Gold bonus.  cryptoList를 반복하고 각 항목에 대해 CryptoCard를 생성하는 메서드를 작성
 
   @override
   Widget build(BuildContext context) {
@@ -153,34 +169,76 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+          // Gold 1. Padding위젯을 CryptoCard라는 별도의 stateless위젯으로 refactoring하여 각 암호화폐마다 3개씩 카드를 만들 수 있음
+          // Gold 2. selectedCurrency, value, CryptoCurrency를 CryptoCard 위젯 생성자에게 전달해야 함
+          // Gold 3. 3개의 CryptoCard포함하기 위해 Column위젯 사용하기
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CryptoCard(
+                cryptoCurrency: 'BTC',
+                // Gold 7-3. 3항연산자를 사용하여 대기 상태를 확인하고, 대기 상태는 ?, 완료 상태는 실제 가격 데이터를 표시
+                value: isWaiting ? '?' : coinValues['BTC']!, // null-safety
+                selectedCurrency: selectedCurrency,
               ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  // Silver 5. 통화를 선택하면 화면에 선택된 통화가 나오도록 하기
-                  '1 BTC = $bitcoinValue $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20.0, color: Colors.white),
-                ),
+              CryptoCard(
+                cryptoCurrency: 'ETH',
+                value: isWaiting ? '?' : coinValues['ETH']!,
+                selectedCurrency: selectedCurrency,
               ),
-            ),
+              CryptoCard(
+                cryptoCurrency: 'DOGE',
+                value: isWaiting ? '?' : coinValues['DOGE']!,
+                selectedCurrency: selectedCurrency,
+              ),
+            ],
           ),
           Container(
-            height: 150.0,
+            height: 200.0,
             alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
+            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
             color: Colors.lightBlue,
             // 3항연산 사용한 플랫폼 분류
             child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+// Gold 1. Padding위젯을 CryptoCard라는 별도의 stateless위젯으로 refactoring하여 각 암호화폐마다 3개씩 카드를 만들 수 있음
+class CryptoCard extends StatelessWidget {
+// Gold 2. selectedCurrency, value, CryptoCurrency를 CryptoCard 위젯 생성자에게 전달해야 함
+  const CryptoCard({
+    required this.value,
+    required this.selectedCurrency,
+    required this.cryptoCurrency,
+  });
+
+  final String value;
+  final String selectedCurrency;
+  final String cryptoCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            // Silver 5. 통화를 선택하면 화면에 선택된 통화가 나오도록 하기
+            '1 $cryptoCurrency = $value $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20.0, color: Colors.white),
+          ),
+        ),
       ),
     );
   }
